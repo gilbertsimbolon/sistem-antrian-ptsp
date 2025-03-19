15fd77cbc9f3984de3d2331fee721fc46acfa6c2
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2025-03-19 11:00:22 +0800

    create model meja pidana

--- app/Models/meja_pidana.php
+++ app/Models/meja_pidana.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class meja_pidana extends Model
 {
-    use HasFactory
+    use HasFactory;
+
+    protected $fillable = [
+        'nomor_perkara',
+        'nama_penggugat',
+        'nama_tergugat',
+        'kuasa_hukum_penggugat',
+        'kuasa_hukum_tergugat',
+        'ruang_sidang',
+        'hakim',
+        'panitera',
+        'tanggal_sidang',
+        'waktu_sidang',
+        'status',
+    ];
 }
