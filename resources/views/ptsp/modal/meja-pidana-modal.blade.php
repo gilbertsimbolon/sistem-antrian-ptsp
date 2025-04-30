46da4982b3657c058fb0e91b260663d3e85d72b5
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2025-04-30 14:14:03 +0800

    add print view and route setting

--- resources/views/ptsp/modal/meja-pidana-modal.blade.php
+++ resources/views/ptsp/modal/meja-pidana-modal.blade.php
@@ -1,7 +1,8 @@
 <!-- Modal -->
-<div class="modal fade" id="modal-pidana" tabindex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+<div class="modal fade" id="modal-inzage" tabindex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-        <form action="/ptsp/antrian" method="POST">
+        <form action="{{ route('antrian.pidana.store') }}" method="POST">
+            @csrf
             <!-- Ganti dengan route sesuai kebutuhan -->
             <div class="modal-content" style="width: 550px">
                 <div class="modal-header">
@@ -14,8 +15,13 @@
                         <input type="text" class="form-control" id="nama" name="nama" required>
                     </div>
                     <div class="mb-3">
-                        <label for="telepon" class="form-label">No. Telepon</label>
-                        <input type="text" class="form-control" id="telepon" name="telepon" required>
+                        <label for="no_telepon" class="form-label">No. Telepon</label>
+                        <input type="text" class="form-control" id="telepon" name="no_telepon" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="keperluan" class="form-label">Keperluan</label>
+                        <textarea class="form-control" id="keperluan" name="keperluan" rows="5" style="resize: none;"
+                            required></textarea>
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Jenis Kelamin</label><br>
@@ -39,3 +45,25 @@
         </form>
     </div>
 </div>
+
+@if(session('modalCetak'))
+<div class="modal fade show" style="display: block;" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-body">
+                @include('ptsp.cetak.cetak-pidana', [
+                'data' => session('data'),
+                'nomor' => session('nomor'),
+                'qrCode' => session('qrCode'),
+                ])
+            </div>
+            <div class="modal-footer">
+                {{-- <button onclick="window.print()" class="btn btn-primary">Cetak</button> --}}
+                <a href="{{ url('/ptsp/antrian/show/' . session('data')->id) }}" class="btn btn-success">Cetak</a>
+                <a href="{{ route('antrian.index') }}" class="btn btn-secondary">Tutup</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal-backdrop fade show"></div>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\MejaPidanaController;
-use App\Http\Controllers\ptsp\AntrianController;
-use App\Http\Controllers\ptsp\DaftarAntrianController;
-use App\Http\Controllers\ptsp\MejaInzageController;
-use App\Http\Controllers\ptsp\pegawai\InzageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ptsp\AntrianController;
+use App\Http\Controllers\ptsp\MejaInzageController;
+use App\Http\Controllers\ptsp\MejaPidanaController;
+use App\Http\Controllers\ptsp\DaftarAntrianController;
+use App\Http\Controllers\ptsp\pegawai\InzageController;
 
 
 // Halaman Login
@@ -24,19 +24,30 @@ Route::middleware('auth')->group(function () {
     // Antrian PTSP
     Route::get('/ptsp/antrian', [AntrianController::class, 'index'])->name('antrian.index');
     Route::post('/ptsp/antrian/store-inzage', [AntrianController::class, 'storeInzage'])->name('antrian.inzage.store');
+    Route::post('/ptsp/antrian/store-pidana', [AntrianController::class, 'storePidana'])->name('antrian.pidana.store');
 
-    // Daftar Antrian MejaInzage
+    // Daftar Antrian Meja Inzage
     Route::get('/ptsp/pegawai/meja-inzage', [InzageController::class, 'index'])->name('pegawai.inzage.index');
+
+    // Daftar Antrian Meja Pidana
+    Route::get('ptsp/pegawai/meja-pidana', [Pidana])
 
     // Meja Inzage
     Route::get('/ptsp/meja-inzage', [MejaInzageController::class, 'index'])->name('meja-inzage.index');
     Route::get('/ptsp/inzage/{id}', [MejaInzageController::class, 'editInzage'])->name('inzage.edit');
     Route::put('/ptsp/inzage/{id}', [MejaInzageController::class, 'update'])->name('inzage.update');
     Route::delete('/ptsp/inzage/{id}', [MejaInzageController::class, 'destroy'])->name('inzage.destroy');
+
+    // Meja Pidana
+    Route::get('/ptsp/meja-pidana', [MejaPidanaController::class, 'index'])->name('meja-pidana.index');
+    Route::get('/ptsp/pidana/{id}', [MejaPidanaController::class, 'editPidana'])->name('pidana.edit');
+    Route::put('/ptsp/pidana/{id}', [MejaPidanaController::class, 'update'])->name('pidana.update');
+    Route::delete('/ptsp/pidana/{id}', [MejaPidanaController::class, 'destroy'])->name('pidana.destroy');
 });
 
 // Route untuk menampilkan antrian tertentu
 Route::get('/ptsp/antrian/show/{id}', [AntrianController::class, 'showInzage'])->name('antrian.inzage.show');
+Route::get('/ptsp/antrian/show/{id}', [AntrianController::class, 'showPidana'])->name('antrian.pidana.show');
 
 // Menggunakan auth.php untuk menangani login, register, dll
 require __DIR__.'/auth.php';
