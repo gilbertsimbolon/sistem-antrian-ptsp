253adc3667372d85707fa688c5c7c0a2cbe6625b
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2025-06-10 11:28:25 +0800

    membuat seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,71 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Admin
-
         User::create([
-            'name' => 'Admin 1 PN Tondano',
-            'email' => 'admin1@pntondano.go.id',
-            'password' => Hash::make('admin1'),
-            'role' => 'admin',
-        ]);
-        User::create([
-            'name' => 'Admin 2 PN Tondano',
-            'email' => 'admin2@pntondano.go.id',
-            'password' => Hash::make('admin2'),
-            'role' => 'admin',
-        ]);
-
-        // Pegawai
-        User::create([
-            'name' => 'Umum',
-            'email' => 'umum@pntondano.go.id',
-            'password' => Hash::make('umum'),
-            'role' => 'staff'
-        ]);
-        User::create([
-            'name' => 'Inzage',
-            'email' => 'inzage@pntondano.go.id',
-            'password' => Hash::make('inzage'),
-            'role' => 'staff',
-        ]);
-        User::create([
-            'name' => 'Pidana',
-            'email' => 'pidana@pntondano.go.id',
-            'password' => Hash::make('pidana'),
-            'role' => 'staff'
-        ]);
-        User::create([
-            'name' => 'Perdata',
-            'email' => 'perdata@pntondano.go.id',
-            'password' => Hash::make('perdata'),
-            'role' => 'staff'
-        ]);
-        User::create([
-            'name' => 'Hukum',
-            'email' => 'hukum@pntondano.go.id',
-            'password' => Hash::make('hukum'),
-            'role' => 'staff',
-        ]);
-        User::create([
-            'name' => 'e-Court',
-            'email' => 'e-court@pntondano.go.id',
-            'password' => Hash::make('e-court'),
-            'role' => 'staff'
-        ]);
-
-        // Operator
-        User::create([
-            'name' => 'Operator 1',
-            'email' => 'operator1@pntondano.go.id',
-            'password' => Hash::make('operator1'),
-            'role' => 'operator',
-        ]);
-        User::create([
-            'name' => 'Operator 2',
-            'email' => 'operator2@pntondano.go.id',
-            'password' => Hash::make('operator2'),
-            'role' => 'operator'
+            'name' => 'bolonsite',
+            'email' => 'bolonsite@pntondano.go.id',
+            'password' => Hash::make('bolonsite'),
         ]);
     }
 }
